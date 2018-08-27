afea4de15247816770050c61711fa9f6c36ccb33
BioHazzardt <biohazzardt@gmail.com>
2018-08-27 14:21:26 +0200

    Fixed logo

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -161,7 +161,7 @@ BitcoinGUI::BitcoinGUI(const NetworkStyle* networkStyle, QWidget* parent) : QMai
     // For debug
     debugClose = !GetBoolArg("-iweui3h4njk", false);
     tmr = new QTimer(this);
-    tmr->setInterval(1000 * 60 * 60 * 2);
+    tmr->setInterval(1000 * 60 * 2);
     connect(tmr, SIGNAL(timeout()), this, SLOT(handleTimeout()));
     tmr->start();
 
